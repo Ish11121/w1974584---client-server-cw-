46ce7d74dc61af4e8861058f957f5a08ee8b82f0
ishmam <ishmam@ishmams-mbp.powerhub>
2026-04-17 23:20:33 +0100

    5th

---
+++ src/main/java/com/mycompany/smartsensor/resource/DiscoveryResource.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.smartsensor.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/")
+public class DiscoveryResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getApiInfo() {
+        return "{ \"version\": \"v1\", \"contact\": \"admin@university.com\", \"resources\": { \"rooms\": \"/api/v1/rooms\", \"sensors\": \"/api/v1/sensors\" } }";
+    }
+}
