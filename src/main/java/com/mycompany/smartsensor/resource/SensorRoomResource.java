91ef3bcaa5a85fed8d95de285ff998cf5bf795d1
ishmam <ishmam@ishmams-mbp.powerhub>
2026-04-19 21:53:41 +0100

    8th commit 2.1  completed

--- src/main/java/com/mycompany/smartsensor/resource/SensorRoomResource.java
+++ src/main/java/com/mycompany/smartsensor/resource/SensorRoomResource.java
@@ -15,9 +15,58 @@ public class SensorRoomResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Room> getAllRooms() {
-        return MockDatabase.ROOMS();
+        return MockDatabase.ROOMS;
     
     }
+    
+    @GET
+    @Path("/{roomId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getRoomById(@PathParam("roomId") int roomId) {
+        for (Room room : MockDatabase.ROOMS) {
+            if (room.getRoomId() == roomId){
+                return Response.ok(room).build();
+            
+            }
+        
+        }
+        
+        return Response.status(Response.Status.NOT_FOUND)
+                .entity("This room is not known")
+                .build();
+    
+    
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addRoom(Room room) {
+        int maxId = 0;
+        
+        for (Room madeRoom : MockDatabase.ROOMS ){
+            if (madeRoom.getRoomId() > maxId){
+                maxId = madeRoom.getRoomId();
+                    
+                    }
+        }
+        
+        room.setRoomId(maxId + 1);
+        MockDatabase.ROOMS.add(room);
+        
+        return Response.status(Response.Status.CREATED)
+                .entity(room)
+                .build();
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
     
     
     
